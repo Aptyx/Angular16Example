1dfd00ac3c26c7b9d5fe3824059a7331c002e6c1
Anthony Batchelor <amb@aptyx.co.uk>
2023-05-19 11:20:23 +0100

    Single Button Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+declare var CrComLib: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular16App';
+
+    // Find our button in the HMTL rendered view and create a local reference.
+    @ViewChild('myButton') myButton!: ElementRef;
+
+    // Tasks to perform when our app (component) loads
+    ngAfterViewInit() {
+      // Obtain the feedback Join number that was set as an HTML Attribute
+      let fbJoin = this.myButton.nativeElement.getAttribute('fbJoin');
+      // On initialisation get the current state of the button from the Control System.
+      this.btnFb(CrComLib.getState('b', String(fbJoin)));
+      // Subscribe for future changes to the button state.
+      CrComLib.subscribeState('b', String(fbJoin), (v: any) => { this.btnFb(v); } );
+    }
+  
+    // Digital press function
+    btnPress(join: any) {
+      CrComLib.publishEvent('b', String(join), true);
+      CrComLib.publishEvent('b', String(join), false);
+    }
+  
+    // Digital feedback function.
+    btnFb(fb:boolean) {
+      let button = this.myButton.nativeElement;
+      if(fb){
+        if(!button.classList.contains('active')){
+          button.classList.add('active');
+        }
+      }
+      else{
+        if(button.classList.contains('active')){
+          button.classList.remove('active');
+        }
+      }
+    }
 }
